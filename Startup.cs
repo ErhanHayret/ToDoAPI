58109da026eb7e6b0d09bbaef5794cd03c26bc32
ErhanHayret <erhan.durgun@outlook.com>
2022-02-04 10:59:41 +0300

    Allow CORS

--- Startup.cs
+++ Startup.cs
@@ -48,6 +48,12 @@ namespace ToDoAPI
 
             app.UseRouting();
 
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
